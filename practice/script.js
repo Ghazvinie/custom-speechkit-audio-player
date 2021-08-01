3b97701f3e3aa92506b0948307146eb1ff1682b4
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-01 09:53:06 +0100

    trying requests

---
+++ practice/script.js
@@ -0,0 +1,38 @@
+import keys from './key.js';
+
+const article = {
+    title: `Test Upload API`,
+    body: `Charlotte Worthington ensured a dramatic start to Sunday's Olympic action with a thrilling gold medal in the women's BMX park freestyle final, while Duncan Scott continued to help Great Britain make history in the pool.
+    Worthington fell on her first run but landed a ground-breaking 360-degree backflip on her second to score 97.50.
+    Declan Brooks then took bronze in the men's event, GB's fourth BMX medal.
+    GB's eighth Tokyo swimming medal came with men's 4x100m medley relay silver.
+    That makes it their most successful Games and means Scott has won more medals at a single Olympics than any other British competitor.
+    The 24-year-old, who signs off with a gold and three silvers, was part of a quartet including Adam Peaty, Luke Greenbank and James Guy that were pipped to the title by the USA.
+    British boxer Ben Whittaker also guaranteed himself at least a silver medal by beating Imam Khataev, of the Russian Olympic Committee, to reach Tuesday's 81kg final. 
+    Pat McCormack will also fight for gold in the men's welterweight division after Ireland's Aidan Walsh pulled out of their semi-final with an ankle injury, while Frazer Clarke reached the super-heavyweight final after France's Mourad Aliev was disqualified.`,
+    author: 'BBC',
+}
+
+async function sendAudio() {
+
+    const fetchParams = {
+        method: 'POST',
+        body: {
+            api_key: keys.api_key,
+            title: article.title,
+            body: article.body, 
+            author: article.author
+        }
+    }
+
+    try {
+        const response = await fetch(`https://app.speechkit.io/api/v3/projects/${keys.project_id}/audio`, fetchParams)
+        console.log(response)
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+sendAudio()
