dabad0074f567f372f832d52e2f71f785472e728
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-04 19:02:07 +0100

    set up player instance

--- player-attempts/1/src/components/Player.js
+++ player-attempts/1/src/components/Player.js
@@ -1,6 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import keys from '../keys';
+import { SpeechKitSdk } from '@speechkit/speechkit-audio-player-v2';
+
+const initParams = {
+  projectId: keys.project_id,
+  externalId: keys.external_id,
+  // UIenabled: true,
+  // renderNode: 'speechkit-player',
+  // isIframe: true
+};
+
 function Player() {
+  const [playerInstance, setPlayerInstance] = useState(null);
+
+  useEffect(() => {
+    async function getPlayer() {
+      const instance = await SpeechKitSdk.player(initParams);
+      setPlayerInstance(instance);
+    };
+    return getPlayer();
+  }, []);
+
   return (
-    <h1>Mello</h1>
+    <div className='player-container'>
+
+    </div>
+
   );
 }
 
---
+++ player-attempts/1/src/speechkit-inst/player-inst.js
@@ -0,0 +1,22 @@
+// import keys from '../keys.js';
+// import { SpeechKitSdk } from '@speechkit/speechkit-audio-player-v2';
+
+// // window.addEventListener('message', event => {
+// //     console.log('event -> data -> ', event.data)
+// // }, false);
+
+// const initParams = {
+//     projectId: keys.project_id,
+//     externalId: keys.external_id,
+//     articleUrl: 'http://http://localhost:3000/',
+//     // UIenabled: true,
+//     // renderNode: 'speechkit-player',
+//     // isIframe: true
+// };
+
+
+// let playerInstance = SpeechKitSdk.player(initParams).then(appInst => playerInstance = appInst);
+
+
+
+// export default playerInstance;
