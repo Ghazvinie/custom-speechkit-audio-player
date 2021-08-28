ad4e306b1490dd781cd496ed5ed05ff3521e48b7
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-28 17:13:36 +0100

    adding player attempt 2

---
+++ player-attempts/2 /src/components/Dropdown.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Dropdown.css'
+
+function Dropdown() {
+    return (
+        <div className='dropdown-container' style={{backgroundColor: 'red'}}>
+            <h2>Like what you hear?</h2>
+            <h3>Subscribe to hear this article and more</h3>
+            <button className='signup-btn' value='hello'>Subscribe</button>
+            <p className='signin-or'>or</p>
+            <button className='signup-btn' value='hello'>Sign In</button>
+        </div>
+    );
+}
+
+export default Dropdown;
